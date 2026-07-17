18ea11b0ae997cee61e3b945f87695511dab0c5d
huangcanda <954060834@qq.com>
2026-07-17 21:48:58 +0800

    Optimize retval module

--- spring-web/src/main/java/io/springperf/web/core/retval/MethodReturnValueContext.java
+++ spring-web/src/main/java/io/springperf/web/core/retval/MethodReturnValueContext.java
@@ -7,6 +7,13 @@ public class MethodReturnValueContext {
     protected MethodParameter returnType;
     protected ReturnValueResolver returnValueResolver;
 
+    /** 异步类型的泛型参数类型，如 DeferredResult<String> 中的 String */
+    protected MethodParameter innerReturnType;
+    /** 匹配 innerReturnType 的解析器 */
+    protected ReturnValueResolver innerReturnValueResolver;
+    /** 是否为异步返回类型 */
+    protected boolean asyncType;
+
     public MethodParameter getReturnType() {
         return returnType;
     }
@@ -21,5 +28,29 @@ public class MethodReturnValueContext {
 
     public void setReturnValueResolver(ReturnValueResolver returnValueResolver) {
         this.returnValueResolver = returnValueResolver;
+    }
+
+    public MethodParameter getInnerReturnType() {
+        return innerReturnType;
+    }
+
+    public void setInnerReturnType(MethodParameter innerReturnType) {
+        this.innerReturnType = innerReturnType;
+    }
+
+    public ReturnValueResolver getInnerReturnValueResolver() {
+        return innerReturnValueResolver;
+    }
+
+    public void setInnerReturnValueResolver(ReturnValueResolver innerReturnValueResolver) {
+        this.innerReturnValueResolver = innerReturnValueResolver;
+    }
+
+    public boolean isAsyncType() {
+        return asyncType;
+    }
+
+    public void setAsyncType(boolean asyncType) {
+        this.asyncType = asyncType;
     }
 }
--- spring-web/src/test/java/io/springperf/web/core/retval/MethodReturnValueContextTest.java
+++ spring-web/src/test/java/io/springperf/web/core/retval/MethodReturnValueContextTest.java
@@ -8,8 +8,7 @@ import org.springframework.core.MethodParameter;
 
 import java.lang.reflect.Method;
 
-import static org.junit.jupiter.api.Assertions.assertNull;
-import static org.junit.jupiter.api.Assertions.assertSame;
+import static org.junit.jupiter.api.Assertions.*;
 
 class MethodReturnValueContextTest {
 
@@ -44,6 +43,49 @@ class MethodReturnValueContextTest {
         ctx.setReturnValueResolver(resolver);
 
         assertSame(resolver, ctx.getReturnValueResolver());
+    }
+
+    // ===== 新增字段 =====
+
+    @Test
+    void innerReturnType_initialState_isNull() {
+        MethodReturnValueContext ctx = new MethodReturnValueContext();
+        assertNull(ctx.getInnerReturnType());
+    }
+
+    @Test
+    void setAndGetInnerReturnType() throws Exception {
+        MethodParameter mp = new MethodParameter(getClass().getMethod("dummy"), -1);
+        MethodReturnValueContext ctx = new MethodReturnValueContext();
+        ctx.setInnerReturnType(mp);
+        assertSame(mp, ctx.getInnerReturnType());
+    }
+
+    @Test
+    void innerReturnValueResolver_initialState_isNull() {
+        MethodReturnValueContext ctx = new MethodReturnValueContext();
+        assertNull(ctx.getInnerReturnValueResolver());
+    }
+
+    @Test
+    void setAndGetInnerReturnValueResolver() {
+        ReturnValueResolver resolver = mockResolver();
+        MethodReturnValueContext ctx = new MethodReturnValueContext();
+        ctx.setInnerReturnValueResolver(resolver);
+        assertSame(resolver, ctx.getInnerReturnValueResolver());
+    }
+
+    @Test
+    void asyncType_initialState_isFalse() {
+        MethodReturnValueContext ctx = new MethodReturnValueContext();
+        assertFalse(ctx.isAsyncType());
+    }
+
+    @Test
+    void setAsyncType_toTrue() {
+        MethodReturnValueContext ctx = new MethodReturnValueContext();
+        ctx.setAsyncType(true);
+        assertTrue(ctx.isAsyncType());
     }
 
     private ReturnValueResolver mockResolver() {
